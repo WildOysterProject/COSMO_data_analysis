2d29e38457da9e3ce5b24ea630ccf7d080f36fd8
jivan <jkhakee@sfsu.edu>
2026-08-24 19:43:48 -0700

    08.24.26 Update
    Updated with all COSMO data aside from one (Jan 2026)
    
    Upgraded Density Plot with Box and Mussel Counts added
    
    Some code consolidation

--- COSMO.R
+++ COSMO.R
@@ -8,28 +8,18 @@ library(vegan)
 
 ####Input####
 COSMO_Input <-
-  read_xlsx("Input/COSMO Data Entry MASTER 08.16.2026.xlsx", sheet = 1)
+  read_xlsx("Input/COSMO Data Entry MASTER 08.24.2026.xlsx", sheet = 1)
 
 ####Processing####
 #####Initial Processing#####
 COSMO_Processed <- COSMO_Input %>%
-  mutate(Live_Oysters = as.numeric(Live_Oysters),
-         oys_per_m2 = Live_Oysters*4,
-         Box_Oysters = as.numeric(Box_Oysters),
-         box_per_m2 = as.numeric(Box_Oysters)*4,
-         Mussel_Count = as.numeric(Mussel_Count),
-         mussel_per_m2 = as.numeric(Mussel_Count)*4,
-         Primary_percent_cover = as.numeric(Primary_percent_cover),
-         Total_percent_cover = as.numeric(Total_percent_cover),
+  mutate(across(Live_Oysters:Total_percent_cover, ~as.numeric(.x)),
          Meter = as.numeric(Meter),
          Site = factor(Site, levels = Site_List),
          Date = openxlsx::convertToDate(Date),
          Year = format(Date, "%Y"),
          totalPrimaryLiveCover = Primary_percent_cover-as.numeric(Bare)) %>% 
   unite("Transect_Meter", Transect_Replicate, Meter, sep = ".") %>% 
-  relocate(oys_per_m2, .after = Live_Oysters) %>% 
-  relocate(box_per_m2, .after = Box_Oysters) %>% 
-  relocate(mussel_per_m2, .after = Mussel_Count) %>% 
   relocate(Year, .after = Date) %>% 
   relocate(totalPrimaryLiveCover, .after = `Total_percent_cover`)
 
@@ -38,6 +28,18 @@ COSMO_Processed_Filtered <- COSMO_Processed %>%
   mutate(SampleID = row_number()) %>% 
   relocate(SampleID, .before = Date) %>% 
   select(-c(`Oyster 1 (mm)`:`Oyster 10 (mm)`))
+
+#####Count Density#####
+
+Cosmo_Count_Density_Pivoted <- COSMO_Processed_Filtered %>% 
+  dplyr::select(SampleID:Live_Mussels) %>% 
+  pivot_longer(cols = Live_Oysters:Live_Mussels,
+               names_to = "Sessile Organism",
+               values_to = "Count") %>% 
+  mutate(Density = Count/`Oyster_count_quadrat_length_(m)`,
+        `Sessile Organism` = factor(sub("_", " ", `Sessile Organism`)),
+        `Sessile Organism` = fct_relevel(`Sessile Organism`, Count_List))
+  
 
 #####Percent Cover#####
 COSMO_Normalized_Percentage <- COSMO_Processed_Filtered %>% 
@@ -65,7 +67,9 @@ Percent_Cover_aggregated <- Percent_Cover_unpivoted_trimmed %>%
     `Bryozoan spp.` = rowSums(dplyr::select(., all_of(`Bryozoan spp.`)), na.rm = T),
     `Bryozoan spp. canopy` = rowSums(dplyr::select(., all_of(`Bryozoan canopy spp.`)), na.rm = T),
     `Tunicate spp.` = rowSums(dplyr::select(., all_of(`Tunicate spp.`)), na.rm = T),
-    `Tunicate spp. canopy` = rowSums(dplyr::select(., all_of(`Tunicate canopy spp.`)), na.rm = T)) %>% 
+    `Tunicate spp. canopy` = rowSums(dplyr::select(., all_of(`Tunicate canopy spp.`)), na.rm = T),
+    `Sponge spp.` = rowSums(dplyr::select(., all_of(`Sponge spp.`)), na.rm = T),
+    `Sponge spp. canopy` = rowSums(dplyr::select(., all_of(`Sponge canopy spp.`)), na.rm = T)) %>% 
   dplyr::select(-c(
     all_of(`Green Algae spp.`), 
     all_of(`Green Algae spp. canopy`), 
@@ -75,6 +79,8 @@ Percent_Cover_aggregated <- Percent_Cover_unpivoted_trimmed %>%
     all_of(`Bryozoan canopy spp.`), 
     all_of(`Tunicate spp.`), 
     all_of(`Tunicate canopy spp.`), 
+    all_of(`Sponge spp.`), 
+    all_of(`Sponge canopy spp.`), 
     `UNID_spp.`
     )) %>% 
   relocate(`Green Algae spp.`:`Tunicate spp. canopy`, .after = Bare)
@@ -116,7 +122,7 @@ perquad_Native_Summary <- Primary_Percent_Cover_aggregated_pivoted %>%
   mutate(NativeProportion = Native/CoverSum,
          `Non-nativeProportion` = `Non-native`/CoverSum,
          UnknownProportion = Unknown/CoverSum) %>% 
-  filter(!is.nan(NativeProportion))%>% 
+  filter(!is.nan(NativeProportion)) %>% 
   group_by(Year, Site) %>% 
   summarise(
     MeanNativeProportion = mean(NativeProportion),
@@ -137,7 +143,7 @@ perquad_Native_Summary <- Primary_Percent_Cover_aggregated_pivoted %>%
 #####Oyster Sizes#####
 COSMO_OysterSizes <- COSMO_Processed %>%
   dplyr::select(!(oys_per_m2:Notes)) %>%
-  filter(Data_Recorder != "SUM") %>% 
+  filter(!(Data_Recorder %in% c("SUM", "TOTAL"))) %>% 
   dplyr::select(!c(`Error_Score_(Qualitative)`:Live_Oysters)) %>% 
   mutate(Transect_Meter = replace_values(Transect_Meter, "NA.NA" ~ NA),
          Substrate_Type = replace_values(Substrate_Type, "N/A" ~ NA)) %>%
@@ -180,6 +186,7 @@ Primary_perquad_Percent_Cover_Native_Summary_barplot <- perquad_Native_Summary %
   theme(axis.text = element_text(size = 12)) +
   coord_cartesian(ylim = c(0,100))
 Primary_perquad_Percent_Cover_Native_Summary_barplot
+ggsave(path = "Plots", filename = "Per-Quadrat Native Summary Barplot.jpg", width = 10, height = 10)
 
 #####Oyster Size Histogram#####
 Oyster_Size_Histogram <- COSMO_OysterSizes %>% 
@@ -197,26 +204,28 @@ Oyster_Size_Histogram <- COSMO_OysterSizes %>%
   facet_grid(Site ~ Year, scales = "free_y") +
   plot_layout(guides = "collect", axes = "collect")
 Oyster_Size_Histogram
+ggsave(path = "Plots", filename = "Oyster Size Histogram.jpg", width = 10, height = 10)
 
-#####Oyster Density Boxplot#####
-Oys_Density_Boxplot <- COSMO_Processed_Filtered %>% 
+
+#####Count Density Boxplot#####
+Count_Density_Boxplot <- Cosmo_Count_Density_Pivoted %>% #Add Box oysters
   ggplot(mapping = aes(
     x = Site,
-    y = oys_per_m2
+    y = Density,
+    fill = `Sessile Organism`
   )
   ) +
-  geom_boxplot(aes(fill = Site), outlier.shape = NA, alpha = 0.7) +
-  scale_fill_manual(limits = Site_List, values = sitecolors) +
-  geom_jitter(color = "darkgrey", height = 0, width = 0.3, alpha = 0.5) + 
-  stat_summary(fun.y="mean", shape = 5, size = 0.4, position = position_dodge(0.55), color = "black")+
-  labs(y = bquote("Oysters/"*m^2)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.9) +
+  scale_fill_manual(limits = Count_List, values = countdensitycolors) +
+  geom_jitter(color = "darkgrey", alpha = 0.5, position = position_jitterdodge(jitter.width = 0.3, dodge.width = 0.75)) + 
+  stat_summary(fun.y="mean", shape = 5, size = 0.4, position = position_dodge(0.75), color = "black")+
+  labs(y = bquote("Organisms/"*m^2)) +
   basic_plot_aesthetics() +
-  theme(legend.position = "none") +
-  facet_grid(~ Year, scales = "free_y") +
-  plot_layout(guides = "collect", axes = "collect") +
-  coord_cartesian(ylim = c(0, 200))
-Oys_Density_Boxplot
-ggsave(path = "Plots", filename = "Oyster Abundance vs. Site Boxplot.jpg", width = 10, height = 10)
+  facet_grid( ~ Year, scales = "free") +
+  theme(legend.position = "bottom") +
+  coord_cartesian(ylim = c(0, 250))
+Count_Density_Boxplot
+ggsave(path = "Plots", filename = "Sessile Organism Density vs. Site Boxplot.jpg", width = 10, height = 10)
 
 #####Primary Percent Cover vs Site Boxplot#####
 Site_Primary_Percent_Cover_Boxplot <- Primary_Percent_Cover_aggregated_pivoted %>%
@@ -253,7 +262,7 @@ Percent_Cover_diversity <- Percent_Cover_unpivoted_trimmed %>%
   pivot_longer(cols = Bare:UNID_spp.,
                names_to = "response_variable",
                values_to = "percent_cover") %>%
-  mutate(percent_cover = percent_cover/4) %>% 
+  mutate(percent_cover = percent_cover/4) %>% #Error score not taken out here?
   rename(spcount = percent_cover) %>%
   group_by(Site, Year, Transect_Meter) %>% 
   summarize(rich = specnumber(spcount),
@@ -313,8 +322,6 @@ Diversity_Combined_Plot
 ggsave(path = "Plots", filename = "Combined Diversity Indices 2025-2026.jpg", width = 8, height = 6)
 
 
-
-
 ####Lists####
 #####Sites#####
 Site_List <- c(
@@ -325,15 +332,24 @@ Site_List <- c(
   "YBI" #Yerba Buena Island
 )
 
+#####Counts#####
+Count_List <- c(
+  "Live Oysters",
+  "Box Oysters",
+  "Live Mussels"
+)
+
 #####Aggregated Taxa Groups#####
-`Green Algae spp.` <- c("Ulva", "Green_Algae_sp.", "Green_filamentous_algae", "Cladophora", "Bryopsis")
-`Green Algae spp. canopy` <- c("Ulva_canopy", "Green_alg_sp._canopy", "Green_filamentous_algae_canopy", "Cladophora_canopy")
-`Red Algae spp.` <- c("Mastocarpus", "Red_Algae_sp.", "Caulacanthus", "Chondracanthus", "Polyneura", "Red_filamentous_algae", "Fucus_spp.", "Gracilaria_spp.", "Cryptopleura_violacea", "Gymnogongrus_spp.", "Polysiphonia_spp.")
-`Red Algae spp. canopy` <- c("Mastocarpus_canopy", "Red_alg_sp._canopy", "Caulacanthus_canopy", "Chondracanthus_canopy", "Polyneura_canopy", "Red_filamentous_algae_canopy", "Crytopleura_canopy", "Mazzaela_splendens_canopy", "Fucus_canopy", "Gracilaria_canopy", "Gymnogongrus_spp_canopy", "Polysiphonia_canopy")
+`Green Algae spp.` <- c("Ulva_spp.", "Ulva_intestinalis", "Green_Algae_sp.", "Green_filamentous_algae", "Cladophora", "Bryopsis")
+`Green Algae spp. canopy` <- c("Ulva_canopy", "Ulva_intestinalis_canopy", "Green_alg_sp._canopy", "Green_filamentous_algae_canopy", "Cladophora_canopy")
+`Red Algae spp.` <- c("Mastocarpus", "Red_Algae_sp.", "Caulacanthus", "Chondracanthus", "Polyneura", "Red_filamentous_algae", "Fucus_spp.", "Gracilaria_spp.", "Cryptopleura_ruprechtiana", "Cryptopleura_spp.", "Gymnogongrus_spp.", "Polysiphonia_spp.")
+`Red Algae spp. canopy` <- c("Mastocarpus_canopy", "Red_alg_sp._canopy", "Caulacanthus_canopy", "Chondracanthus_canopy", "Polyneura_canopy", "Red_filamentous_algae_canopy", "Cryptopleura_ruprechtiana_canopy", "Cryptopleura_spp_canopy", "Mazzaela_splendens_canopy", "Fucus_canopy", "Gracilaria_canopy", "Gymnogongrus_spp_canopy", "Polysiphonia_canopy")
 `Bryozoan spp.` <- c("Bryozoan", "Bugula_neritina", "Cryptosula_pallasiana", "Watersipora_spp.", "Schizoporella_spp.", "Encrusting_bryozoan_spp.", "Upright_bryozoan_spp.")
 `Bryozoan canopy spp.` <- c("Bryozoan_canopy", "Bugula_neritina_canopy", "Encrusting_bryozoan_canopy", "Upright_bryozoan_canopy")
-`Tunicate spp.` <- c("Tunicate", "Colonial_tunicate", "Solitary_tunicate")
+`Tunicate spp.` <- c("Tunicate", "Colonial_tunicate", "Solitary_tunicate", "Botrylloides_spp.")
 `Tunicate canopy spp.` <- c("Colonial_tunicate_canopy")
+`Sponge spp.` <- c("Sponge", "Halichondria_spp.")
+`Sponge canopy spp.` <- c("Sponge_canopy")
 
 #####Colors#####
 ######Aggregated Taxa######
@@ -367,5 +383,13 @@ nativecolors <- c(
   `Native` = tempColors4[1],
   `Non-native` = tempColors4[2],
   `Unknown` = "purple")
+
+#######Native vs. Non-native######
+tempColors5 <- mako(3, begin = 0.3, end = 0.8)
+countdensitycolors <- c(
+  `Live Oysters` = tempColors5[1],
+  `Box Oysters` = tempColors5[2],
+  `Live Mussels` = tempColors5[3])
+
 
 #knitr::spin("COSMO.R", format = "Rmd", knit = FALSE)
